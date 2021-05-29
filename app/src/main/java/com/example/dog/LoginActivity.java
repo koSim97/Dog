2d7abedbff6fecc3b05e34ec0ec2f1493f2316ac
Sim <simjh323@naver.com>
2021-05-29 15:09:32 +0900

    [심지훈]파이어베이스 연동 및 구글로그인 설정만 적용

--- app/src/main/java/com/example/dog/LoginActivity.java
+++ app/src/main/java/com/example/dog/LoginActivity.java
@@ -4,11 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends TitleActivity {
+
+    FirebaseAuth firebaseAuth;
+    private GoogleApiClient mGoogleApiClient;
+    private static final int RC_SIGN_IN = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //구글로그인 설정
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("264591587999-ieogm2s3bqo13a7au4cct0acg1n3cl43.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, (GoogleApiClient.OnConnectionFailedListener) this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
+                .build();
+
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 }
--- app/src/main/java/com/example/dog/MainActivity.java
+++ app/src/main/java/com/example/dog/MainActivity.java
@@ -2,11 +2,20 @@ package com.example.dog;
 
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends TitleActivity {
+
+    FirebaseAuth firebaseAuth;
+    private GoogleApiClient mGoogleApiClient;
+    private static final int RC_SIGN_IN = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
