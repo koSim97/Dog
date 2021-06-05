9368629bbbb98b3b9dc9caed00a0443831a875eb
Sim <simjh323@naver.com>
2021-06-05 14:56:08 +0900

    [심지훈]파이어베이스 연동 및 구글로그인 적용 완료

--- app/src/main/java/com/example/dog/LoginActivity.java
+++ app/src/main/java/com/example/dog/LoginActivity.java
@@ -1,26 +1,49 @@
 package com.example.dog;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.annotations.NotNull;
 
-public class LoginActivity extends TitleActivity {
 
-    FirebaseAuth firebaseAuth;
-    private GoogleApiClient mGoogleApiClient;
-    private static final int RC_SIGN_IN = 100;
+public class LoginActivity extends TitleActivity implements GoogleApiClient.OnConnectionFailedListener {
+
     EditText etId;
     EditText etPassword;
     Button btnLogin;
     Button btnJoin;
     Button btnGoogleLogin;
+
+    private static final int RC_SIGN_IN = 100;
+    private FirebaseAuth mAuth;
+    private GoogleApiClient mGoogleApiClient;
+    private SignInButton signInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,18 +54,70 @@ public class LoginActivity extends TitleActivity {
         etPassword = (EditText) findViewById(R.id.login_edit_password);
         btnLogin = (Button) findViewById(R.id.login_btn_login);
         btnJoin = (Button) findViewById(R.id.login_btn_join);
-        btnGoogleLogin = (Button) findViewById(R.id.login_btn_login_google);
+        signInButton = findViewById(R.id.login_btn_login_google);
 
-        //구글로그인 설정
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken("264591587999-ieogm2s3bqo13a7au4cct0acg1n3cl43.apps.googleusercontent.com")
+                .requestIdToken(getString(R.string.google_key))
                 .requestEmail()
                 .build();
         mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this, (GoogleApiClient.OnConnectionFailedListener) this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
 
-        firebaseAuth = FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                startActivityForResult(signInIntent,RC_SIGN_IN);
+            }
+        });
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Log.d("test","2");
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            Log.d("test","2"+result.toString());
+            if (result.isSuccess()) {
+                GoogleSignInAccount account = result.getSignInAccount();
+                Log.d("test","3");
+                firebaseAuthWithGoogle(account);
+            } else {
+                Log.d("test","4");
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d("test","1");
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "인증 실패", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "구글 로그인 인증 성공", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+
+    @Override
+    public void onConnectionFailed(@NotNull ConnectionResult connectionResult) {
+
     }
 }
--- app/src/main/java/com/example/dog/MainActivity.java
+++ app/src/main/java/com/example/dog/MainActivity.java
@@ -1,16 +1,13 @@
 package com.example.dog;
 
+import android.content.Context;
 import android.os.Bundle;
 
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.firebase.auth.FirebaseAuth;
+
 
 public class MainActivity extends TitleActivity {
 
-    FirebaseAuth firebaseAuth;
-    private GoogleApiClient mGoogleApiClient;
-    private static final int RC_SIGN_IN = 100;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
