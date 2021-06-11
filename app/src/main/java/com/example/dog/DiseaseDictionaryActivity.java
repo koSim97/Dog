6004726c05051dae2b314257e855444f7e264928
umseozz <eum4494@naver.com>
2021-06-11 15:27:57 +0900

    Update DiseaseDictionaryActivity

--- app/src/main/java/com/example/dog/DiseaseDictionaryActivity.java
+++ app/src/main/java/com/example/dog/DiseaseDictionaryActivity.java
@@ -14,8 +14,55 @@ public class DiseaseDictionaryActivity extends AppCompatActivity {
     TextView tvResult;
     XmlPullParser xpp;
 
+    String apiURL = "https://https://api.odcloud.kr/api/15050442/v1/uddi:825b02de-f7ad-469a-b0ff-91b8e958c2df_201909241343";
     String key = "mThyOrnHDnGoGU%2BMOvk3pyfQVqW9EbCckwlVmK%2FR17XLWcRmloRRSdNFQ7X7GmZOBfKFAdhO1RnQuOW%2FGfrj7w%3D%3D";
     String data;
+
+    String disName;
+    String livestock;
+    String disDefinition;
+    String symptom;
+    String cure;
+
+    public String getDisName() {
+        return disName;
+    }
+
+    public void setDisName(String disName) {
+        this.disName = disName;
+    }
+
+    public String getLivestock() {
+        return livestock;
+    }
+
+    public void setLivestock(String livestock) {
+        this.livestock = livestock;
+    }
+
+    public String getDisDefinition() {
+        return disDefinition;
+    }
+
+    public void setDisDefinition(String disDefinition) {
+        this.disDefinition = disDefinition;
+    }
+
+    public String getSymptom() {
+        return symptom;
+    }
+
+    public void setSymptom(String symptom) {
+        this.symptom = symptom;
+    }
+
+    public String getCure() {
+        return cure;
+    }
+
+    public void setCure(String cure) {
+        this.cure = cure;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +71,7 @@ public class DiseaseDictionaryActivity extends AppCompatActivity {
 
         etSearch = (EditText)findViewById(R.id.dis_edit_search);
         tvResult = (TextView)findViewById(R.id.dis_text_result);
+
+
     }
 }
--- app/src/main/java/com/example/dog/MainActivity.java
+++ app/src/main/java/com/example/dog/MainActivity.java
@@ -7,8 +7,6 @@ import android.os.Bundle;
 
 public class MainActivity extends TitleActivity {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
