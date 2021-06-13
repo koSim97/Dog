5e4df0748bd7b603ccf6030be7e00aa9d3a7eaeb
umseohyun <eum4494@naver.com>
2021-06-13 17:43:16 +0900

    Update PetProfile

--- app/src/main/java/com/example/dog/PetProfileActivity.java
+++ app/src/main/java/com/example/dog/PetProfileActivity.java
@@ -7,31 +7,42 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class PetProfileActivity extends AppCompatActivity {
 
 
-    Button btnUpdate;
-    Button btnBack;
+    private TextView tvName;
+    private TextView tvAge;
+    private Button btnUpdate;
+    private Button btnBack;
+
     private FirebaseDatabase FD;
     private DatabaseReference DR;
     private FirebaseAuth firebaseAuth;
     private FirebaseUser user;
     private String uid;
 
+    private String pName;
+    private String pAge;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet_profile);
 
+        tvName = (TextView)findViewById(R.id.petprof_text_name);
+        tvAge = (TextView)findViewById(R.id.petprof_text_age);
         btnUpdate = (Button)findViewById(R.id.petprof_btn_update);
         btnBack = (Button)findViewById(R.id.petprof_btn_back);
 
@@ -41,15 +52,19 @@ public class PetProfileActivity extends AppCompatActivity {
         user = firebaseAuth.getCurrentUser();
         uid = user.getUid();
 
-        DR.child(uid).child("Pet").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+        DR.child("Users").child(uid).child("Pet").addValueEventListener(new ValueEventListener() {
             @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if(!task.isSuccessful()){
-                    Log.e("FireBase","Failed to Load", task.getException());
-                }
-                else{
-                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
-                }
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                pName = snapshot.child("pName").getValue(String.class);
+                pAge  = snapshot.child("pAge").getValue(String.class);
+
+                tvName.setText(pName);
+                tvAge.setText(pAge);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
 
